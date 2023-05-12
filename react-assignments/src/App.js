d29695bac7c35f56842391f2cfe8a3953fbaaf66
AnoopAG91 <anoopag47@gmail.com>
2023-05-12 10:22:24 +0530

    assign13 tic tac toe game

--- react-assignments/src/App.js
+++ react-assignments/src/App.js
@@ -1,11 +1,65 @@
-import ProfileCard from "./components/profileCard";
+import { useState } from "react";
+import Board from "./components/Board"
 
 function App() {
+
+  const WinConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+  const [scores, setScores] = useState({xScore:0, oScore:0});
+
+  const handleBoxClick = (boxIndex) => {
+  
+  const upDateBoard = board.map((value, index)=>{
+    if (index === boxIndex){
+      return xPlaying === true ? "X" : "O";
+    }else{
+      return value;
+    }
+  })
+
+
+
+  const winner = checkWinner(upDateBoard)
+  if(winner){
+    if (winner === "O"){
+      let {oScore} = scores;
+    }else 
+  }
+  setBoard(upDateBoard);
+
+  setXPlaying(!xPlaying);
+
+ }
+
+const checkWinner = (board) =>{
+  for(let i =0; i < WinConditions.length; i++){
+    const [x,y,z] = WinConditions[i];
+
+    if(board[x] && board[x] === board[y] && board[y] === board[z]){
+      console.log(board[x]);
+      return board[x];
+    }
+  }
+}
+
   return (
     <div className="App">
-      < ProfileCard/>
+     
+     <Board board={board} onClick={handleBoxClick}/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ react-assignments/src/components/Board.js
@@ -0,0 +1,14 @@
+import './Board.css'
+import Box from "./Box";
+
+const Board = ({board, onClick}) => {
+    return(
+        <div className='board'>
+            {board.map((value, index)=> {
+                return <Box value={value} onClick={()=> value === null && onClick(index)}/>
+            })}
+        </div>
+    )
+}
+
+export default Board;
