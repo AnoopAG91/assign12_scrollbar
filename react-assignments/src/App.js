69bf0df502413100aa424a7a2b3317aca024317f
AnoopAG91 <anoopag47@gmail.com>
2023-05-13 23:14:31 +0530

    assign14_todoapp

--- react-assignments/src/App.js
+++ react-assignments/src/App.js
@@ -1,61 +1,15 @@
-import { useState } from "react";
-import Board from "./components/Board"
+import Todo from './components/TodoApp/Todo'
 
 function App() {
-
-  const WinConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-
-
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xPlaying, setXPlaying] = useState(true);
-  const [scores, setScores] = useState({xScore:0, oScore:0});
-
-  const handleBoxClick = (boxIndex) => {
-  
-  const upDateBoard = board.map((value, index)=>{
-    if (index === boxIndex){
-      return xPlaying === true ? "X" : "O";
-    }else{
-      return value;
-    }
-  })
-
-
-
-   checkWinner(upDateBoard)
- 
-  setBoard(upDateBoard);
-
-  setXPlaying(!xPlaying);
-
- }
-
-const checkWinner = (board) =>{
-  for(let i =0; i < WinConditions.length; i++){
-    const [x,y,z] = WinConditions[i];
-
-    if(board[x] && board[x] === board[y] && board[y] === board[z]){
-      console.log(board[x]);
-      return board[x];
-    }
-  }
-}
 
   return (
     <div className="App">
      
-     <Board board={board} onClick={handleBoxClick}/>
+     <Todo/>
+     
     </div>
   )
+
 }
 
-export default App;
+export default App;                       
